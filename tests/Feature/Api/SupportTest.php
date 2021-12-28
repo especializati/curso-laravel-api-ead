ad4421aea2b920047495b261868af550333875f0
Carlos Ferreira <conta@especializati.com.br>
2021-12-28 10:32:05 -0300

    Tests Supports and Filter Lesson

--- tests/Feature/Api/SupportTest.php
+++ tests/Feature/Api/SupportTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\Lesson;
 use App\Models\Support;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -35,5 +36,39 @@ class SupportTest extends TestCase
 
         $response->assertStatus(200)
                     ->assertJsonCount(50, 'data');
+    }
+
+    public function test_get_supports_unauthenticated()
+    {
+        $response = $this->getJson('/supports');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_get_supports()
+    {
+        Support::factory()->count(50)->create();
+
+        $response = $this->getJson('/supports', $this->defaultHeaders());
+
+        $response->assertStatus(200)
+                    ->assertJsonCount(50, 'data');
+    }
+
+    public function test_get_supports_filter_lesson()
+    {
+        $lesson = Lesson::factory()->create();
+
+        Support::factory()->count(50)->create();
+        Support::factory()->count(10)->create([
+            'lesson_id' => $lesson->id,
+        ]);
+
+        $payload = ['lesson' => $lesson->id];
+
+        $response = $this->json('GET', '/supports', $payload, $this->defaultHeaders());
+
+        $response->assertStatus(200)
+                    ->assertJsonCount(10, 'data');
     }
 }
